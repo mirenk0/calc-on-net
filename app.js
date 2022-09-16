834f6020fb847bd769da9af62da19cf082ea5555
mirenk0 <mirenkodaci21@gmail.com>
2022-09-16 11:03:50 +0200

    Get dom elements

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const clearContent = document.querySelector(".clear");
+const deleteContent = document.querySelector(".delete");
+
+const numberBtn = document.querySelectorAll("[data-number]");
+const operatorBtn = document.querySelectorAll("[data-operator]");
+const pointBtn = document.querySelector("#point-btn");
+const equalBtn = document.querySelector("#equal-btn");
+const display = document.querySelector(".display");
 
 
 
