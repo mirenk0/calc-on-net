3c01a35e25f68adc1d2ca6be2218976089cce4c7
mirenk0 <mirenkodaci21@gmail.com>
2022-09-16 10:48:56 +0200

    Add basic calculation functions

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+
+function add(fnum, snum){
+    return fnum + snum;
+}
+
+function subtract(fnum, snum){
+    return fnum - snum;
+}
+
+function division(fnum, snum){
+    return fnum / snum;
+}
+
+function mul(fnum, snum) {
+    return fnum * snum;
+}
+
+
+
