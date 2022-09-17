b4756c1f0c1a74c2d6a47a0620cebb776c01a62b
mirenk0 <mirenkodaci21@gmail.com>
2022-09-17 11:55:54 +0200

    Add event listener actions to operands and operators

--- app.js
+++ app.js
@@ -6,6 +6,49 @@ const operatorBtn = document.querySelectorAll("[data-operator]");
 const pointBtn = document.querySelector("#point-btn");
 const equalBtn = document.querySelector("#equal-btn");
 const display = document.querySelector(".display");
+display.innerHTML = null;
+
+const operands = Array.from(numberBtn);
+const operators = Array.from(operatorBtn);
+
+//We get all the buttons
+//We listen for an click event to happen to any of them to get their value 
+//Then we display the value in our display 
+//If we've clicked a specific operator activate the operate function to it when = is clicked
+//Results are returned as the first operand now
+//Need to add the new operator and the second operand
+
+
+//Pressing keyboard keys
+window.addEventListener("keydown", (e) => {
+    if(e.key >= 48 && e.key <= 57){
+        display.innerHTML = e;
+    }
+});
+
+
+const operatorAction = operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        return operator.innerHTML;
+    })
+});
+
+const operandsAction = operands.forEach(operand => {
+    operand.addEventListener("click", () => {
+        return operand.innerHTML;
+    })
+});
+
+
+const results = equalBtn.addEventListener("click", () => {
+    return operate(operatorAction, operandsAction, operandsAction);
+});
+
+
+//Display with onclick
+function displayChange(ele){
+    display.innerHTML += ele.innerHTML;
+}
 
 
 //operate
@@ -31,7 +74,16 @@ function operate(operator, fnum, snum) {
 }
 
 
+//Functions
 
+
+function clear() {
+    display.innerHTML = null;
+}
+
+function deletion() {
+
+}
 
 function add(fnum, snum){
     return fnum + snum;
